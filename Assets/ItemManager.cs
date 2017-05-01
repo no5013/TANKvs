a44decb0d694f01c3f75dcca7d8ff5561bd2f28e
Pongsakorn Somsri <pongsakorns@gmail.com>
2017-05-02 02:11:40 +0700

    Add item spawn

---
+++ Assets/ItemManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour {
+
+	public Transform[] itemSpawnPoints;
+
+	public GameObject[] items;
+
+	public float spawnTime = 10f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+
+	// Update is called once per frame
+	void Spawn () {
+		int spawnPointIndex = Random.Range (0, itemSpawnPoints.Length);
+		int itemIndex = Random.Range (0, items.Length);
+
+		print (spawnPointIndex);
+		print (itemIndex);
+
+		Instantiate (items [itemIndex], itemSpawnPoints [spawnPointIndex].position, itemSpawnPoints [spawnPointIndex].rotation);
+	}
+}
